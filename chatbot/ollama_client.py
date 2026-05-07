0a3c4e525a30e12ceedd60049e34cc2818e0fcde
Rizqi Asan Masika <rizqiasanmm@gmail.com>
2026-05-07 14:39:03 +0700

    bikin biar ollama jalan di ngrok

--- chatbot/ollama_client.py
+++ chatbot/ollama_client.py
@@ -1,7 +1,9 @@
 import requests
 
 def call_ollama(prompt: str, system: str):
-    response = requests.post("http://localhost:11434/api/chat", json={
+    NGROK_URL = " https://3a87-2402-8780-1065-8907-785e-907-21e-7554.ngrok-free.app" 
+    
+    response = requests.post(f"{NGROK_URL}/api/chat", json={
         "model": "gemma3:4b",
         "messages": [
             {"role": "system", "content": system},
