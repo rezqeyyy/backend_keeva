af7cb858adbcabcb127dc1aee176aedcd3b41129
Rizqi Asan Masika <rizqiasanmm@gmail.com>
2026-05-07 15:53:43 +0700

    update typo

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        "https://keeva-ai.vercel.app",
+        "https://keeva-ai.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
