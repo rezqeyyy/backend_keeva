84f37a3310f7f0f3e71f8660a43e114c8f807a80
Rizqi Asan Masika <rizqiasanmm@gmail.com>
2026-05-07 15:59:02 +0700

    tes origins

--- main.py
+++ main.py
@@ -16,11 +16,7 @@ app = FastAPI()
 # Enable CORS for Frontend connectivity
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
-        "https://keeva-ai.vercel.app"
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
