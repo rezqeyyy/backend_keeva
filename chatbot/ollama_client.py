6c5270e2eb9c85ae449daad326a9809835151588
Rizqi Asan Masika <rizqiasanmm@gmail.com>
2026-05-07 14:41:25 +0700

    benerin typo

--- chatbot/ollama_client.py
+++ chatbot/ollama_client.py
@@ -1,7 +1,7 @@
 import requests
 
 def call_ollama(prompt: str, system: str):
-    NGROK_URL = " https://3a87-2402-8780-1065-8907-785e-907-21e-7554.ngrok-free.app" 
+    NGROK_URL = "https://3a87-2402-8780-1065-8907-785e-907-21e-7554.ngrok-free.app" 
     
     response = requests.post(f"{NGROK_URL}/api/chat", json={
         "model": "gemma3:4b",
